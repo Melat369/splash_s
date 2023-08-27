d5ba11235328bfd0fd3cbcf819624043f3a0aaf6
Melat369 <melattesfaye16@gmail.com>
2023-08-27 16:21:36 +0300

    finalized splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:splash_s/screens/splash_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(const MyApp());
 }
 
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import '../home/home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +12,13 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+
+    super.initState();
+    Timer(Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder:(context)=>HomePage())))
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
