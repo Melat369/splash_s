4f4f0a40b77ea7610495430f11462af0bddafabc
Melat369 <melattesfaye16@gmail.com>
2023-08-27 16:01:55 +0300

    added components to the splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -11,7 +11,38 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+    body:Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomRight,
+          colors: [Color(0xffE19898),
+                   Color(0xffA2678A)
+                   ]
+                 ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children:[
+                                                Image.asset('assets\images\logo.png', height:300, width:300),
+                      Text('Tech News', 
+                       textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          ),),
+                        ]
+                      ),
+                      SizedBox(height:50),
+                      CircularProgressIndicator(),
+                    ],                  
+                ),
+               )
 
     );
   }
