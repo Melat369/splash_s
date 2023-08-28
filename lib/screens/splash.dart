ba606ca0b906b689c382d1d938ed3a428b15fe10
Melat369 <melattesfaye16@gmail.com>
2023-08-28 20:37:20 +0300

    new splash design

---
+++ lib/screens/splash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashS extends StatefulWidget {
+  const SplashS ({super.key});
+
+  @override
+  State<SplashS > createState() => _SplashS State();
+}
+
+class _SplashS State extends State<SplashS > {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
